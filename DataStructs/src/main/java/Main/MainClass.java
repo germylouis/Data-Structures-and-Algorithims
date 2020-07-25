abfd6783278d238af7f8e38c7e43ca2c64faf9db
germl <germy.louis@enhanceit.us>
2020-07-24 21:31:48 -0400

    CS

--- DataStructs/src/main/java/Main/MainClass.java
+++ DataStructs/src/main/java/Main/MainClass.java
@@ -1,10 +1,34 @@
 package Main;
 
 
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
 public class MainClass {
 
     public static void main(String[] args) {
+        String line = "hello";
+        StringBuilder string = new StringBuilder();
+        Map<Character, Integer> map = new LinkedHashMap<>();
+        char[] arr = line.toCharArray();
+        Arrays.sort(arr);
+
+        for (char c : arr) {
+            if (!map.containsKey(c)) {
+                map.put(c, 1);
+            } else {
+                map.replace(c, map.get(c) + 1);
+            }
+        }
+
+        map.forEach((k,v) -> string.append(k.toString() + v.toString()));
+
+        System.out.println(string);
 
     }
 
+    public void test() {
+
+    }
 }
--- DataStructs/src/main/java/RandomCodeChallenges/StringIncrement.java
+++ DataStructs/src/main/java/RandomCodeChallenges/StringIncrement.java
@@ -13,4 +13,23 @@ public class StringIncrement {
         System.out.println(sb);
     }
 
+    public static void incrementedLogic(String str){
+
+        StringBuilder sb = new StringBuilder();
+        String z = "z";
+        String Z = "Z";
+        for (int i = 0; i < str.length(); i++) {
+            System.out.println(str.charAt(i));
+
+            if(z.equals(str.charAt(i))){
+               str.replace(str.charAt(i), 'a');
+            }
+
+            sb.append((char) (str.charAt(i) + 1));
+            System.out.println(sb);
+
+        }
+        System.out.println(sb);
+    }
+
 }
