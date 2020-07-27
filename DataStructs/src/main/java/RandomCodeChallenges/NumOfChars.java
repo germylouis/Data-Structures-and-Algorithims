ba44140e8e86d6757c04ce2f0bdab0c07cd05c05
Germy Louis <germylouis@gmail.com>
2020-07-26 22:33:32 -0400

    NumChars and OnlyOnce Classes added.

---
+++ DataStructs/src/main/java/RandomCodeChallenges/NumOfChars.java
@@ -0,0 +1,28 @@
+package main.java.RandomCodeChallenges;
+
+import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class NumOfChars {
+
+    public static void charCount(String line){
+        StringBuilder string = new StringBuilder();
+        Map<Character, Integer> map = new LinkedHashMap<>();
+        char[] arr = line.toCharArray();
+        Arrays.sort(arr);
+
+        for (char c : arr) {
+
+            if (!map.containsKey(c)) {
+                map.put(c, 1);
+            } else {
+                map.replace(c, map.get(c) + 1);
+            }
+        }
+
+        map.forEach((k,v) -> string.append(k.toString() + v.toString()));
+
+        System.out.println(string);
+    }
+}
